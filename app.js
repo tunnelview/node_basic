1dd8a75fcca8c5b3acead271b6499b6058dfe1fc
tunnelview <86519065+tunnelview@users.noreply.github.com>
2022-05-07 10:20:58 +1000

    Add sample function

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 console.log("This is my first node code, yayyy");
 console.log("window");
 
-const a = 6;
-const b = 5;
+// const a = 6;
+// const b = 5;
 
-const result = a + b;
+// const result = a + b;
+function add(a, b) {
+  return a + b;
+}
 
-console.log(result);
+console.log(add(5, 6));
